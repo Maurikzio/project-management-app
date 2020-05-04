7b0cce055e181ad780aa097f6e5661f644575fd3
Maurikzio <m.morocho1196@gmail.com>
2020-05-04 23:19:09 +0300

    Updates

---
+++ precache-manifest.6f91dea3648f823eeca01a1db0e33856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb2b994b46d396f1967649a6516340a",
+    "url": "/project-management-app/index.html"
+  },
+  {
+    "revision": "84be15f58ab5df6c40df",
+    "url": "/project-management-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d612b5decc061c459f32",
+    "url": "/project-management-app/static/js/2.d81cd9b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-management-app/static/js/2.d81cd9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84be15f58ab5df6c40df",
+    "url": "/project-management-app/static/js/main.556b97de.chunk.js"
+  },
+  {
+    "revision": "70773cd7afa20c71904c",
+    "url": "/project-management-app/static/js/runtime-main.bac507a8.js"
+  }
+]);
