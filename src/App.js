2fe39dd5e67c5383153448a39738db68504ed908
Maurikzio <m.morocho1196@gmail.com>
2020-05-18 02:43:27 +0300

    adding createproject component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
           <Route exact path='/project-management-app' component={Dashboard}/>
           <Route path='/project-management-app/project/:id' component={ProjectDetails}/>
           <Route path='/project-management-app/signin' component={SignIn}/>
-          <Route path='/project-management-app/signup' component={SignUp} />
+          <Route path='/project-management-app/signup' component={SignUp}/>
+          <Route path='/project-management-app/create' component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
