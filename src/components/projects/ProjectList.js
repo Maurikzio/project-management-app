5babd7d70ffeaafdae5fce6f2ae5fb83d12dad1c
Maurikzio <m.morocho1196@gmail.com>
2020-05-17 03:39:34 +0300

    uploading project summary

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,30 +1,18 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary'
 import styled from 'styled-components';
 
-const ListContainer = styled.div`
+const ProjectsContainer = styled.div`
     flex-grow: 3;
     border: 1px solid red;
 `
 
 const ProjectList = () => {
     return (
-        <ListContainer>
-            <div>
-                <span>Project Title1</span>
-                <p>Posted by: Shadow Ninja</p>
-                <p>16th November 2020</p>
-            </div>
-            <div>
-                <span>Project Title2</span>
-                <p>Posted by: Shadow Ninja 2</p>
-                <p>16th November 2020</p>
-            </div>
-            <div>
-                <span>Project Title3</span>
-                <p>Posted by: Shadow Ninja 3</p>
-                <p>16th November 2020</p>
-            </div>
-        </ListContainer>
+        <ProjectsContainer>
+            <ProjectSummary/>
+            <ProjectSummary/>
+        </ProjectsContainer>
     )
 }
 
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div>
+            <span>Project Title1</span>
+            <p>Posted by: Shadow Ninja</p>
+            <p>16th November 2020</p>
+        </div>
+    )
+}
+export default ProjectSummary;
